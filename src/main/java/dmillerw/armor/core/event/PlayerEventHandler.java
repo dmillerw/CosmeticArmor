fcfea3f8194c19759c83e750c6713c233591209a
dmillerw <dmillerw@gmail.com>
2014-11-27 12:18:27 -0800

    Sync armor on player login

--- src/main/java/dmillerw/armor/core/event/PlayerEventHandler.java
+++ src/main/java/dmillerw/armor/core/event/PlayerEventHandler.java
@@ -1,8 +1,13 @@
 package dmillerw.armor.core.event;
 
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import cpw.mods.fml.server.FMLServerHandler;
 import dmillerw.armor.core.handler.PlayerHandler;
+import dmillerw.armor.core.inventory.InventoryArmor;
+import dmillerw.armor.core.network.PacketHandler;
+import dmillerw.armor.core.network.packet.PacketSyncArmor;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraftforge.event.entity.player.PlayerDropsEvent;
 import net.minecraftforge.event.entity.player.PlayerEvent;
 
@@ -10,6 +15,26 @@ import net.minecraftforge.event.entity.player.PlayerEvent;
  * @author dmillerw
  */
 public class PlayerEventHandler {
+
+    @SubscribeEvent
+    public void onPlayerLoggedIn(cpw.mods.fml.common.gameevent.PlayerEvent.PlayerLoggedInEvent event) {
+        if (!event.player.worldObj.isRemote) {
+            for (int i=0; i<4; i++) {
+                // Send logged in Player's armor to all other players
+                PlayerHandler.getArmor(event.player).syncSlotToClients(i);
+
+                // Send all other Player's armor to logged in player
+                for (Object obj : FMLServerHandler.instance().getServer().getConfigurationManager().playerEntityList) {
+                    InventoryArmor inventoryArmor = PlayerHandler.getArmor((EntityPlayer) obj);
+                    PacketSyncArmor packetSyncArmor = new PacketSyncArmor();
+                    packetSyncArmor.entityId = ((EntityPlayer)obj).getEntityId();
+                    packetSyncArmor.slot = i;
+                    packetSyncArmor.itemStack = inventoryArmor.getStackInSlot(i);
+                    PacketHandler.INSTANCE.sendTo(packetSyncArmor, (EntityPlayerMP) obj);
+                }
+            }
+        }
+    }
 
     @SubscribeEvent
     public void onPlayerDeath(PlayerDropsEvent event) {
